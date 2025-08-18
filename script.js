3f9f0ce011d4254dd2a6aedc55cee26b238d0e17
Michael Talapila <mikhailtalapila@yahoo.com>
2025-08-18 19:54:53 -0400

    added reset form functionality

--- script.js
+++ script.js
@@ -15,10 +15,26 @@
 //    document.querySelector('.message').textContent = 'No number';
 //  }
 //});
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
+const generateSecretNumber = function () {
+  return Math.trunc(Math.random() * 20) + 1;
+};
+let score = 20;
+let secretNumber = generateSecretNumber();
+let highScore = 0;
+const startNewGame = function () {
+  score = 20;
+  secretNumber = generateSecretNumber();
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = null;
+  document.querySelector('.message').textContent = 'Start guessing...';
+};
+startNewGame();
+
+document.querySelector('.again').addEventListener('click', startNewGame);
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
@@ -27,6 +43,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Correct number!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       score--;
