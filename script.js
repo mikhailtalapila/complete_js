83363bb5775da5a916d553ef9bbec5fd7e273a38
Michael Talapila <mikhailtalapila@yahoo.com>
2025-08-25 21:57:27 -0400

    additional changes

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
 'use strict';
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
-  function printAge() {
-    let output = `${firstName}, You are ${age} born in ${birthYear}`;
-    console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      const firstName = 'mikhail';
-      var millenial = true;
-      const str = `Oh, are are're a millenial, ${firstName}`;
-      console.log(str);
-      function add(a, b) {
-        return a + b;
-      }
-      output = 'new output';
-    }
-    console.log(millenial);
-    // console.log(add(2,3));
-    console.log(output);
-  }
-  printAge();
-  return age;
+console.log(me);
+//console.log(job);
+//console.log(year);
+
+var me = 'Mikhail';
+let job = 'teacher';
+const year = 1995;
+console.log(addDecl(2, 3));
+//console.log(addExpr(4, 5));
+//console.log(addArrow(3, 4));
+console.log(addArrow);
+function addDecl(a, b) {
+  return a + b;
 }
 
-const firstName = 'Jonas';
-calcAge(1991);
-// printAge();
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+//example
+if (!numProducts) {
+  deleteShoppingCart();
+}
+
+var numProducts = 10;
+function deleteShoppingCart() {
+  console.log('all products deleted');
+}
+
+//best practices:
+// use const to declare vars - > not vars / use let if need to change the vars
+// declare the vars at the top of each scope
+// always declare functions before calling them
+// even for func declaration , not just function expressions
+
+var x = 1;
+let y = 2;
+const z = 3;
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
