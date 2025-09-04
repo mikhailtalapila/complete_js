acf07b18ee33fc1d08bfd6124894fcb37c24b4f2
Michael Talapila <mikhailtalapila@yahoo.com>
2025-09-04 18:23:21 -0400

    init

--- script.js
+++ script.js
@@ -1,132 +1,47 @@
 'use strict';
-//console.log(me);
-//console.log(job);
-//console.log(year);
 
-var me = 'Mikhail';
-let job = 'teacher';
-const year = 1991;
-//console.log(addDecl(2, 3));
-//console.log(addExp);
-//console.log(addExp(2, 3));
-//console.log(addArrow(2, 3));
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// function addDecl(a, b) {
-//   return a + b;
-// }
+const italianFoods = new Set([
+  'pasta',
+  'gnocchi',
+  'tomatoes',
+  'olive oil',
+  'garlic',
+  'basil',
+]);
 
-// var addExp = function (a, b) {
-//   return a + b;
-// };
-// var addArrow = (a, b) => a + b;
+const mexicanFoods = new Set([
+  'tortillas',
+  'beans',
+  'rice',
+  'tomatoes',
+  'avocado',
+  'garlic',
+]);
 
-// console.log(numProducts);
-// if (!numProducts) {
-//   deleteShoppingCart();
-// }
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-// var numProducts = 10;
-// function deleteShoppingCart() {
-//   console.log('all products deleted');
-// }
-
-//var x = 2;
-//let y = 1;
-//const xe = 3;
-//console.log(y === window.y);
-//console.log(x === window.x);
-const calcAge = function (birthYear) {
-  //console.log(2037 - birthYear);
-  //console.log(this);
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
 };
-//calcAge(1983);
-
-const calcAgeArrow = birthyear => {
-  //console.log(2037 - birthyear);
-  //console.log(this);
-};
-//calcAgeArrow(1983);
-// const mikhail = {
-//   year: 1983,
-//   calcAge: function () {
-//     console.log(this);
-//     console.log(2037 - this.year);
-//   },
-// };
-// //mikhail.calcAge();
-
-// const jonas = {
-//   year: 2012,
-// };
-// jonas.calcAge = mikhail.calcAge;
-// //jonas.calcAge();
-// const f = jonas.calcAge;
-// console.log(f);
-// f();
-
-// var firstName = 'tammy';
-// const mikhail = {
-//   firstName: 'Mikhail',
-//   year: 1983,
-//   calcAge: function () {
-//     console.log(this);
-//     console.log(2037 - this.year);
-//     // const self = this;
-//     // const isMillenial = function () {
-//     //   console.log(self.year >= 1981 && self.year <= 1996);
-//     // };
-//     // isMillenial();
-//     const isMillenial = () => {
-//       console.log(this.year >= 1981 && this.year <= 1996);
-//     };
-//     isMillenial();
-//   },
-//   greet: function () {
-//     console.log(`hey ${this.firstName}`);
-//     console.log(this);
-//   },
-// };
-// mikhail.greet();
-// //console.log(this.firstName);
-// mikhail.calcAge();
-
-// const addExp = function (a, b) {
-//   console.log(arguments);
-//   return a + b;
-// };
-// //addExp(2, 3);
-// //addExp(2, 3, 4, 5);
-
-// var addArrow = (a, b) => {
-//   //console.log(arguments);
-//   return a + b;
-// };
-
-// addArrow(2, 4);
-// const mikhail = {
-//   firstName: 'Mikhail',
-//   lastName: 'Test',
-//   age: 27,
-// };
-
-// function marryPerson(person, newLastName) {
-//   person.lastName = newLastName;
-//   return person;
-// }
-
-// const newMikhail = marryPerson(mikhail, 'test3');
-// // const otherMikhail = mikhail;
-// // otherMikhail.lastName = 'test2';
-// console.log('before:', mikhail, newMikhail);
-
-const mikhail = {
-  firstName: 'Mikhail',
-  lastName: 'Test',
-  age: 27,
-  family: ['tammy', 'kids'],
-};
-
-const mikhailCopy = { ...mikhail };
-console.log(mikhail);
-mikhail.lastName = 'test2';
-console.log(mikhailCopy);
