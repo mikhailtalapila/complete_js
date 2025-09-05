adc47cccdb3e71e18b8089fe05d8fca62f1db195
Michael Talapila <mikhailtalapila@yahoo.com>
2025-09-05 17:02:38 -0400

    spread operator

--- script.js
+++ script.js
@@ -1,35 +1,342 @@
+// 'use strict';
+
+// // Data needed for a later exercise
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// const italianFoods = new Set([
+//   'pasta',
+//   'gnocchi',
+//   'tomatoes',
+//   'olive oil',
+//   'garlic',
+//   'basil',
+// ]);
+
+// const mexicanFoods = new Set([
+//   'tortillas',
+//   'beans',
+//   'rice',
+//   'tomatoes',
+//   'avocado',
+//   'garlic',
+// ]);
+
+// // Data needed for first part of the section
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+//   order: function (startedindex, mainindex) {
+//     return [this.starterMenu[startedindex], this.mainMenu[mainindex]];
+//   },
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
+// };
+
+// // const arr = [2, 3, 4];
+// // const a = arr[0];
+// // const b = arr[1];
+// // const c = arr[2];
+// // const [x, y, z] = arr;
+
+// // console.log(x, y, z);
+// // console.log(arr);
+// // let [main, , secondary] = restaurant.categories;
+// // console.log(main, secondary);
+// // // const temp = main;
+// // // main = secondary;
+// // // secondary = temp;
+// // [main, secondary] = [secondary, main];
+// // console.log(main, secondary);
+// // const [starter, mainCourse] = restaurant.order(2, 2);
+// // console.log(starter, mainCourse);
+// // const nested = [2, 4, [5, 6]];
+// // // const [i, , j] = nested;
+// // //console.log(i, j);
+
+// // const [i, , [j, k]] = nested;
+// // console.log(i, j, k); //
+// const [p, q, r] = [9, 8];
+// console.log(p, q, r);
 'use strict';
+const books = [
+  {
+    title: 'Algorithms',
+    author: ['Robert Sedgewick', 'Kevin Wayne'],
+    publisher: 'Addison-Wesley Professional',
+    publicationDate: '2011-03-24',
+    edition: 4,
+    keywords: [
+      'computer science',
+      'programming',
+      'algorithms',
+      'data structures',
+      'java',
+      'math',
+      'software',
+      'engineering',
+    ],
+    pages: 976,
+    format: 'hardcover',
+    ISBN: '9780321573513',
+    language: 'English',
+    programmingLanguage: 'Java',
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.41,
+        ratingsCount: 1733,
+        reviewsCount: 63,
+        fiveStarRatingCount: 976,
+        oneStarRatingCount: 13,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: 'Structure and Interpretation of Computer Programs',
+    author: [
+      'Harold Abelson',
+      'Gerald Jay Sussman',
+      'Julie Sussman (Contributor)',
+    ],
+    publisher: 'The MIT Press',
+    publicationDate: '2022-04-12',
+    edition: 2,
+    keywords: [
+      'computer science',
+      'programming',
+      'javascript',
+      'software',
+      'engineering',
+    ],
+    pages: 640,
+    format: 'paperback',
+    ISBN: '9780262543231',
+    language: 'English',
+    programmingLanguage: 'JavaScript',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.36,
+        ratingsCount: 14,
+        reviewsCount: 3,
+        fiveStarRatingCount: 8,
+        oneStarRatingCount: 0,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "Computer Systems: A Programmer's Perspective",
+    author: ['Randal E. Bryant', "David Richard O'Hallaron"],
+    publisher: 'Prentice Hall',
+    publicationDate: '2002-01-01',
+    edition: 1,
+    keywords: [
+      'computer science',
+      'computer systems',
+      'programming',
+      'software',
+      'C',
+      'engineering',
+    ],
+    pages: 978,
+    format: 'hardcover',
+    ISBN: '9780130340740',
+    language: 'English',
+    programmingLanguage: 'C',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 1010,
+        reviewsCount: 57,
+        fiveStarRatingCount: 638,
+        oneStarRatingCount: 16,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: 'Operating System Concepts',
+    author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
+    publisher: 'John Wiley & Sons',
+    publicationDate: '2004-12-14',
+    edition: 10,
+    keywords: [
+      'computer science',
+      'operating systems',
+      'programming',
+      'software',
+      'C',
+      'Java',
+      'engineering',
+    ],
+    pages: 921,
+    format: 'hardcover',
+    ISBN: '9780471694663',
+    language: 'English',
+    programmingLanguage: 'C, Java',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 3.9,
+        ratingsCount: 2131,
+        reviewsCount: 114,
+        fiveStarRatingCount: 728,
+        oneStarRatingCount: 65,
+      },
+    },
+  },
+  {
+    title: 'Engineering Mathematics',
+    author: ['K.A. Stroud', 'Dexter J. Booth'],
+    publisher: 'Palgrave',
+    publicationDate: '2007-01-01',
+    edition: 14,
+    keywords: ['mathematics', 'engineering'],
+    pages: 1288,
+    format: 'paperback',
+    ISBN: '9781403942463',
+    language: 'English',
+    programmingLanguage: null,
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.35,
+        ratingsCount: 370,
+        reviewsCount: 18,
+        fiveStarRatingCount: 211,
+        oneStarRatingCount: 6,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: 'The Personal MBA: Master the Art of Business',
+    author: 'Josh Kaufman',
+    publisher: 'Portfolio',
+    publicationDate: '2010-12-30',
+    keywords: ['business'],
+    pages: 416,
+    format: 'hardcover',
+    ISBN: '9781591843528',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.11,
+        ratingsCount: 40119,
+        reviewsCount: 1351,
+        fiveStarRatingCount: 18033,
+        oneStarRatingCount: 1090,
+      },
+    },
+  },
+  {
+    title: 'Crafting Interpreters',
+    author: 'Robert Nystrom',
+    publisher: 'Genever Benning',
+    publicationDate: '2021-07-28',
+    keywords: [
+      'computer science',
+      'compilers',
+      'engineering',
+      'interpreters',
+      'software',
+      'engineering',
+    ],
+    pages: 865,
+    format: 'paperback',
+    ISBN: '9780990582939',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.7,
+        ratingsCount: 253,
+        reviewsCount: 23,
+        fiveStarRatingCount: 193,
+        oneStarRatingCount: 0,
+      },
+    },
+  },
+  {
+    title: 'Deep Work: Rules for Focused Success in a Distracted World',
+    author: 'Cal Newport',
+    publisher: 'Grand Central Publishing',
+    publicationDate: '2016-01-05',
+    edition: 1,
+    keywords: ['work', 'focus', 'personal development', 'business'],
+    pages: 296,
+    format: 'hardcover',
+    ISBN: '9781455586691',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.19,
+        ratingsCount: 144584,
+        reviewsCount: 11598,
+        fiveStarRatingCount: 63405,
+        oneStarRatingCount: 1808,
+      },
+    },
+    highlighted: true,
+  },
+];
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+//const [firstBook, secondBook] = books;
+//console.log(firstBook, secondBook);
+//
+//
+//const [, , thirdbook] = books;
+//console.log(thirdbook);
+//const ratings = [
+//  ['rating', 4.19],
+//  ['ratingsCount', 144584],
+//];
+//const [[, rating], [, ratingCount]] = ratings;
+//console.log(rating, ratingCount);
+//
+//const ratingStars = [63405, 1808];
+//const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
+//console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
+//
 
-const italianFoods = new Set([
-  'pasta',
-  'gnocchi',
-  'tomatoes',
-  'olive oil',
-  'garlic',
-  'basil',
-]);
-
-const mexicanFoods = new Set([
-  'tortillas',
-  'beans',
-  'rice',
-  'tomatoes',
-  'avocado',
-  'garlic',
-]);
-
-// Data needed for first part of the section
+//const ratings = [323, 23];
+//const [oneStar = 0, twoStar = 0, threeStar = 0, fourStar = 0, fiveStar = 0] =
+//  ratings;
+//console.log(oneStar, twoStar, threeStar, fourStar, fiveStar);
+//
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
+  order: function (startedindex, mainindex) {
+    return [this.starterMenu[startedindex], this.mainMenu[mainindex]];
+  },
+  orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time, address }) {
+    console.log(`order reeived. will be delivered to ${address}`);
+  },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`here is your pasta with ${ing1} ${ing2} ${ing3}`);
+  },
   openingHours: {
     thu: {
       open: 12,
@@ -45,3 +352,77 @@ const restaurant = {
     },
   },
 };
+//restaurant.orderDelivery({
+//  time: '22:30',
+//  address: 'blabla',
+//  mainIndex: 2,
+//  starterIndex: 2,
+//});
+//restaurant.orderDelivery({
+//  address: 'blabla2',
+//  mainIndex: 2,
+//  starterIndex: 2,
+//});
+//const { name, categories, openingHours } = restaurant;
+//console.log(name, categories, openingHours);
+//
+//const { name: restaurantName, openingHours: hours } = restaurant;
+//console.log(restaurantName, hours);
+//
+//const { menu = [], starterMenu: starters = [] } = restaurant;
+//console.log(menu, starters);
+//
+//let a = 111;
+//let b = 999;
+//const obj = { a: 23, b: 7, c: 14 };
+//({ a, b } = obj);
+//console.log(a, b);
+//
+//const { openingHours } = restaurant;
+//console.log(openingHours);
+//const {
+//  fri: { open: o, close: c },
+//} = openingHours;
+//console.log(o, c);
+const arr = [7, 8, 9];
+//const badNewArray = [1, 2, arr[0], arr[1], arr[2]];
+//console.log(badNewArray);
+//
+const newArray = [1, 2, ...arr];
+//console.log(newArray);
+//
+//const newWrongArr = [1, 2, arr];
+//console.log(newWrongArr);
+
+//console.log(...newArray);
+
+//const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+//console.log(...newMenu);
+//
+
+//const mainMenuCopy = [...restaurant.mainMenu];
+//console.log(mainMenuCopy);
+//const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+//console.log(menu);
+//
+
+//const str = 'Jonas';
+//const letters = [...str, ' ', 's'];
+//console.log(...letters);
+
+//const ingregients = [
+//  prompt('Lets make pasta! Ingregient 1? '),
+//  prompt('Ing2'),
+//  prompt('ing3'),
+//];
+//console.log(...ingregients);
+//restaurant.orderPasta(...ingregients);
+
+//Objects
+const newRestaurant = { ...restaurant, founder: 'Guiseppe' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'test2';
+console.log(restaurant);
+console.log(restaurantCopy);
