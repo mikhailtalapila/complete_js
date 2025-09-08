538dfa87941868d9b4cce6313411f81a3c0d7cbd
Michael Talapila <mikhailtalapila@yahoo.com>
2025-09-08 16:31:24 -0400

    completed strings

--- script.js
+++ script.js
@@ -1187,3 +1187,68 @@ GOOD LUCK 😀
 // planesInLine(5);
 // planesInLine(2);
 //
+///////////////////////////////////////
+// Coding Challenge #4
+
+//Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+// The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+//THIS TEST DATA (pasted to textarea)
+//underscore_case
+// first_name
+//Some_Variable
+//  calculate_AGE
+//delayed_departure
+//
+//SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+//underscoreCase      ✅
+//firstName           ✅✅
+//someVariable        ✅✅✅
+//calculateAge        ✅✅✅✅
+//delayedDeparture    ✅✅✅✅✅
+//
+//HINT 1: Remember which character defines a new line in the textarea 😉
+//HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+//HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+//HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+//
+//Afterwards, test with your own test data!
+//
+//GOOD LUCK 😀
+//*/
+//
+
+//document.body.append(document.createElement('textarea'));
+//document.body.append(document.createElement('button'));
+//
+//const text = document
+//.querySelector('button')
+//.addEventListener('click', function () {
+//  const txt = document.querySelector('textarea').value;
+//  const phrases = txt.split('\n');
+//  console.log(phrases);
+//  for (const [i, line] of phrases.entries()) {
+//    const [first, second] = line.trim().split('_');
+//    console.log(i);
+//    console.log(second);
+//    let output = `${first}${second.replace(
+//      second[0],
+//      second[0].toUpperCase()
+//    )}`;
+//    console.log(output.padEnd(20, 'x'.repeat(i)));
+//  }
+//});
+// const getCode = str => str.slice(0, 3).toUpperCase();
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+//
+// // console.log(flights.split('+'));
+// for (const flight of flights.split('+')) {
+//   const [type, from, to, time] = flight.split(';');
+//   const output = `${type.startsWith('_Delayed') ? '*' : ''}${type.replaceAll(
+//     '_',
+//     ' '
+//   )}${getCode(from)}${getCode(to)}${time.replace(':', 'h')}`.padStart(35);
+//   console.log(output);
+// }
