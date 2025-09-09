e08ca925388d089fd06e06a127c173dc00fe3253
Michael Talapila <mikhailtalapila@yahoo.com>
2025-09-09 18:30:09 -0400

    bind function

--- script.js
+++ script.js
@@ -126,3 +126,128 @@
 
 // book.call(swiss, ...flightData);
 // console.log(swiss);
+
+// //bing method
+
+// const bookEW = book.bind(eurowings);
+// const bookLH = book.bind(lufthansa);
+// const bookLX = book.bind(swiss);
+
+// bookEW(23, 'Willaim Smith');
+// const bookEW23 = book.bind(eurowings, 23);
+// bookEW23('Mary Shmoe');
+
+// //with event listeners
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//   console.log(this);
+//   this.planes++;
+//   console.log(this.planes);
+// };
+// // lufthansa.buyPlane();
+
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// //partial application
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+// const addVAT = addTax.bind(null, 0.07);
+// console.log(addVAT(100));
+// console.log(addVAT(450));
+
+// const addTax2 = function (value) {
+//   return function () {
+//     return value + value * 0.07;
+//   };
+// };
+// console.log(addTax2(50)());
+
+// const addTaxRate = function (rate) {
+//   return function (value) {
+//     return value + value * rate;
+//   };
+// };
+
+// const addSalesTax = addTaxRate(0.07);
+// console.log(addSalesTax(100));
+// console.log(addSalesTax(50));
+
+// const buyHat = function (size) {
+//   return function (color) {
+//     return `Buying ${color} hat ${size} size`;
+//   };
+// };
+// console.log(buyHat(4));
+// const buyHatSize7 = buyHat(7);
+// console.log(buyHatSize7('white'));
+
+// const exchangeMoney = function (amount) {
+//   return function (currencyNeeded) {
+//     return `Exchanged ${amount} for ${currencyNeeded}`;
+//   };
+// };
+
+// const exchange100 = exchangeMoney(100);
+// console.log(exchange100('US Dollars'));
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). 
+This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: Javascript', '1: Python', '2: Rust', '3: C++'],
+  answers: new Array(4).fill(0),
+  registerNewAnswer: function () {
+    let q = this.question + '\n';
+    for (const answer of this.options) q += answer + '\n';
+    console.log(q);
+    const a = prompt(q);
+    if (a !== '1' && a !== '2' && a !== '0' && a !== '3')
+      console.error('Invalid input');
+    else this.answers[Number(a)]++;
+    this.displayResults('string');
+  },
+  displayResults: function (type = 'array') {
+    if (type === 'array') console.log(this.answers);
+    if (type === 'string') {
+      let output = 'Poll results are ';
+      for (const a in this.answers) output += a + ', ';
+      console.log(output.slice(0, -2));
+    }
+  },
+};
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
