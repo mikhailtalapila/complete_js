2b41d13c85f3d02162cac9b93915ff5bf515ed65
Michael Talapila <mikhailtalapila@yahoo.com>
2025-09-23 19:35:26 -0400

    reduce

--- script.js
+++ script.js
@@ -5,38 +5,6 @@
 // BANKIST APP
 // FIXME:
 // Data
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [
-    200, 450, -400, 3000, -650, -130, 70, 1300, 1500, 123, 1235, 1234, -500,
-    -700, -989,
-  ],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
-
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
-
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -64,23 +32,6 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
-  containerMovements.innerHTML = '';
-  movements.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdrawal';
-    const html = `
-        <div class="movements__row">
-          <div class="movements__type movements__type--${type}">${
-      i + 1
-    } ${type}</div>
-          <div class="movements__value">${mov}</div>
-        </div>
-    `;
-    containerMovements.insertAdjacentHTML('afterbegin', html);
-  });
-};
-
-displayMovements(account1.movements);
 // console.log(containerMovements.innerHTML);
 
 /////////////////////////////////////////////////
@@ -172,19 +123,154 @@ displayMovements(account1.movements);
 // const data2 = [10, 5, 6, 1, 4];
 // checkDogs(data1, data2);
 //
+//
+//const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+//const EuToUsd = 1.1;
+//const movementsUsd = movements.map(el => el * EuToUsd);
+//console.log(movements);
+//console.log(movementsUsd);
+//
+//let movementsUsdFor = [];
+//for (const mov of movements) {
+//  movementsUsdFor.push(mov * EuToUsd);
+//}
+//console.log(movementsUsdFor);
+//const movementsDescriptions = movements.map((mov, i) => {
+//  return `Movement ${i + 1}:  you ${
+//    mov > 0 ? 'deposited' : 'withdrew'
+//  } ${Math.abs(mov)}`;
+//});
+//console.log(movementsDescriptions);
+//const account1 = {
+//  owner: 'Jonas Schmedtmann',
+//  movements: [
+//    200, 450, -400, 3000, -650, -130, 70, 1300, 1500, 123, 1235, 1234, -500,
+//    -700, -989,
+//  ],
+//  interestRate: 1.2, // %
+//  pin: 1111,
+//};
+//
+//const account2 = {
+//  owner: 'Jessica Davis',
+//  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+//  interestRate: 1.5,
+//  pin: 2222,
+//};
+//
+//const account3 = {
+//  owner: 'Steven Thomas Williams',
+//  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+//  interestRate: 0.7,
+//  pin: 3333,
+//};
+//
+//const account4 = {
+//  owner: 'Sarah Smith',
+//  movements: [430, 1000, 700, 50, 90],
+//  interestRate: 1,
+//  pin: 4444,
+//};
+//
+//const accounts = [account1, account2, account3, account4];
+//const displayMovements = function (movements) {
+//  containerMovements.innerHTML = '';
+//  movements.forEach(function (mov, i) {
+//    const type = mov > 0 ? 'deposit' : 'withdrawal';
+//    const html = `
+//        <div class="movements__row">
+//          <div class="movements__type movements__type--${type}">${
+//      i + 1
+//    } ${type}</div>
+//          <div class="movements__value">${mov}</div>
+//        </div>
+//    `;
+//    containerMovements.insertAdjacentHTML('afterbegin', html);
+//  });
+//};
+//const calcPrintBalance = function (movements) {
+//  const balance = movements.reduce((acc, curr) => acc + curr, 0);
+//  labelBalance.textContent = `$${balance}`;
+//};
+//
+//calcPrintBalance(account1.movements);
+//
+//displayMovements(account1.movements);
+//const createUsernames = function (accts) {
+//  accts.forEach(function (acc) {
+//    acc.username = acc.owner
+//      .toLowerCase()
+//      .split(' ')
+//      .map(el => el[0])
+//      .join('');
+//  });
+//};
+//createUsernames(accounts);
+//console.log(accounts);
+//const deposits = movements.filter(function (el) {
+//  return el > 0;
+//});
+//console.log(deposits);
+//const depositsFor = [];
+//for (const el of movements) if (el > 0) depositsFor.push(el);
+//console.log(depositsFor);
+//const withdrawals = movements.filter(el => el < 0);
+//console.log(withdrawals);
+//console.log(movements);
+//const balance = movements.reduce(function (acc, curr, i, arr) {
+//  console.log(`Current balance: ${acc}$`);
+//  return acc + curr;
+//}, 1000); //initial value of the accumulator
+//console.log(balance);
+//
+//let balance2 = 0;
+//for (const mov of movements) balance2 += mov;
+//console.log(balance2);
+//const balance3 = movements.reduce((acc, curr) => acc + curr, 50);
+//console.log(balance3);
+//
+////Maximum value of the movements array
+//const maxValue = movements.reduce((acc, curr) => {
+//  if (acc > curr) return acc;
+//  else return curr;
+//}, movements[0]);
+//console.log(maxValue);
+//
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-const EuToUsd = 1.1;
-const movementsUsd = movements.map(el => el * EuToUsd);
-console.log(movements);
-console.log(movementsUsd);
+///////////////////////////////////////
+// Coding Challenge #2
 
-let movementsUsdFor = [];
-for (const mov of movements) {
-  movementsUsdFor.push(mov * EuToUsd);
-}
-console.log(movementsUsdFor);
-const movementsDescriptions = movements.map((mov, i, array) => {
-  return `you ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`;
-});
-console.log(movementsDescriptions);
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+*/
+
+// const calcAverageHumanAge = function (ages) {
+//   return ages.map(function (el) {
+//     if (el <= 2) return el * 2;
+//     else return 16 + el * 4;
+//   });
+// };
+// const dogAges1 = [5, 2, 4, 1, 15, 8, 3];
+// const humanAges = calcAverageHumanAge(dogAges1);
+// const filtered = humanAges.filter(el => el > 18);
+// const totalAge = filtered.reduce(function (acc, curr) {
+//   return acc + curr;
+// }, 0);
+// const average = filtered.reduce((acc, curr, i, arr) => {
+//   return acc + curr / arr.length;
+// }, 0);
+// console.log(`total is ${totalAge}`);
+// console.log(`average is ${totalAge / filtered.length}`);
+// console.log(`another average is ${average}`);
+// console.log(humanAges);
+// console.log(filtered);
