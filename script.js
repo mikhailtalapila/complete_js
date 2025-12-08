6fb8b5e0218670ecc0387f13bf1a0fd174cf8f0d
Michael Talapila <mikhailtalapila@yahoo.com>
2025-12-08 17:22:56 -0500

    dom traversing

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ const randomInt = (min, max) =>
 const randomColor = () => `rgb(${randomInt(0, 255)}, 
   ${randomInt(0, 255)},${randomInt(0, 255)}
 )`;
-console.log(randomColor());
+// console.log(randomColor());
 document.querySelector('.nav__link').addEventListener('click', function (e) {});
 document.querySelector('.nav__links').addEventListener('click', function (e) {
   this.style.backgroundColor = randomColor();
@@ -119,3 +119,30 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     });
   }
 });
+// const h1 = document.querySelector('h1');
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// console.log(h1.firstChild);
+// h1.firstElementChild.style.color = 'white';
+// h1.lastElementChild.style.color = 'orangered';
+//
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+// h1.closest('.header').style.background = 'var(--gradient-secondary)';
+// h1.closest('h1').style.background = 'var(--gradient-primary)';
+//
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
+//
+// console.log(h1.parentElement.children);
+// h1.parentElement.children;
+// [...h1.parentElement.children].forEach(function (el) {
+//   if (el !== h1) {
+//     el.style.transform = 'scale(0.5)';
+//   }
+// });
+//
+//
