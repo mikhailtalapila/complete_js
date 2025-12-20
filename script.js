674131c5701c477a565c6fdeeba46d6c2e66cde1
Michael Talapila <mikhailtalapila@yahoo.com>
2025-12-20 07:44:17 -0500

    class style

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 'use strict';
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
-};
-
-const mike = new Person('Mike', 1999);
-console.log(mike);
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
+//
+// const mike = new Person('Mike', 1999);
+// console.log(mike);
 
 //1. new {} is created
 //2. function is called , this = {}
 //3. {} linked to prototype
 //4. function automatically return {}
 
-const john = new Person('John', 1984);
-console.log(john);
-console.log(john instanceof Person);
+// const john = new Person('John', 1984);
+// console.log(john);
+// console.log(john instanceof Person);
 
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-};
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
 // console.log(Person.prototype);
 // john.calcAge();
 //
@@ -55,3 +55,64 @@ Person.prototype.calcAge = function () {
 // console.log(array1.unique2());
 // const h1 = document.querySelector('h1');
 // console.log(x => x + 1);
+//1
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+//
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(this.speed);
+// };
+//
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(this.speed);
+// };
+//
+// const bmw = new Car('bmw', 120);
+// const mercedes = new Car('mercedes', 95);
+//
+// bmw.accelerate();
+// bmw.accelerate();
+// mercedes.accelerate();
+// mercedes.accelerate();
+// bmw.brake();
+// bmw.brake();
+
+//class expression
+// const PersonCl = class {
+//
+// }
+//class declaration
+
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+  calcAge() {
+    console.log(2038 - this.birthYear);
+  }
+  greet = function () {
+    console.log(`hey ${this.firstName}`);
+  };
+  sayGoodbye() {
+    console.log(`goodbye ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1999);
+console.log(jessica);
+jessica.calcAge();
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`hey ${this.firstName}`);
+// };
+jessica.greet();
+console.log(jessica.hasOwnProperty('greet'));
+console.log(jessica.hasOwnProperty('calcAge'));
+//1. classes are not hoisted
+//2. classes are first-class citizens
+//3. classes are executed in strict mode
