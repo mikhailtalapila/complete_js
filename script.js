6c9af54feb8ad264529cda328ede5a909d10523c
Michael Talapila <mikhailtalapila@yahoo.com>
2026-01-13 21:02:26 -0500

    added a form

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
 const form = document.querySelector(".form");
 const containerWorkouts = document.querySelector(".workouts");
 const inputType = document.querySelector(".form__input--type");
@@ -10,6 +9,7 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+let map, mapEvent;
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -25,25 +25,11 @@ if (navigator.geolocation) {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
-
-      map.on("click", function (mapEvent) {
-        const { lat, lng } = mapEvent.latlng;
-        console.log(lat);
-        console.log(lng);
-        console.log(mapEvent);
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: "running-popup",
-            })
-          )
-          .setPopupContent("<b>Workout</b>")
-          .openPopup();
+      //handling click on maps
+      map.on("click", function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
       });
     },
     function () {
@@ -51,3 +37,24 @@ if (navigator.geolocation) {
     }
   );
 }
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  console.log(lat);
+  console.log(lng);
+  console.log(mapEvent);
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoclose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("<b>Workout</b>")
+    .openPopup();
+});
