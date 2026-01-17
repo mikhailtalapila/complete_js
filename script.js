7e294cc42f3d1194cec4c5c9cef0c1d2ee2a0c90
Michael Talapila <mikhailtalapila@yahoo.com>
2026-01-16 21:31:07 -0500

    created workout class

--- script.js
+++ script.js
@@ -10,6 +10,47 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + "").slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+  }
+  calcSpeed() {
+    this.speed = this.distance / this.duration / 60;
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+
+// const cycling = new Cycling([39, -12], 27, 95, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 189);
+// const cycling2 = new Cycling([39, -12], 28, 12, 178);
+// console.log(run1, cycling);
+
+//application architecture
+
 class App {
   #map;
   #mapEvent;
