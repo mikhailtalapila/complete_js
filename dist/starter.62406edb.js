fa058b539803b3801743d1cb8d3aba7bed875d7b
Michael Talapila <mikhailtalapila@yahoo.com>
2026-01-24 17:47:49 -0500

    fetch the first recipe

--- dist/starter.62406edb.js
+++ dist/starter.62406edb.js
@@ -1,43 +1,35 @@
-import * as model from "./model.js";
-import icons from "../img/icons.svg";
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-
-console.log(icons);
 const recipeContainer = document.querySelector(".recipe");
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+const timeout = function(s) {
+    return new Promise(function(_, reject) {
+        setTimeout(function() {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
 };
-
 // NEW API URL (instead of the one shown in the video)
 // https://forkify-api.jonas.io
-const renderSpinner = function (parentEl) {
-  const markup = `
-  <div class="spinner">
-          <svg>
-            <use href="${icons}#icon-loader"></use>
-          </svg>
-        </div>
-  `;
-  parentEl.innerHTML = "";
-  parentEl.insertAdjacentHTML("afterbegin", markup);
-};
 ///////////////////////////////////////
-const showRecipe = async function () {
-  try {
-    const id = window.location.hash.slice(1);
-    renderSpinner(recipeContainer);
-    if (!id) return;
-    // 1. loading recipe
-    await model.loadRecipe(id);
-    const { recipe } = model.state;
-    // 2. rendering recipe
-    const markup = `
+const showRecipe = async function() {
+    try {
+        // 1. loading recipe
+        const res = await fetch("https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e8895");
+        const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+        // console.log(data);
+        let { recipe } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        };
+        console.log(recipe);
+        // 2. rendering recipe
+        const markup = `
     <figure class="recipe__fig">
           <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
           <h1 class="recipe__title">
@@ -47,14 +39,14 @@ const showRecipe = async function () {
     <div class="recipe__details">
           <div class="recipe__info">
             <svg class="recipe__info-icon">
-              <use href="${icons}#icon-clock"></use>
+              <use href="src/img/icons.svg#icon-clock"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
             <span class="recipe__info-text">minutes</span>
           </div>
           <div class="recipe__info">
             <svg class="recipe__info-icon">
-              <use href="${icons}#icon-users"></use>
+              <use href="src/img/icons.svg#icon-users"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
             <span class="recipe__info-text">servings</span>
@@ -62,12 +54,12 @@ const showRecipe = async function () {
             <div class="recipe__info-buttons">
               <button class="btn--tiny btn--increase-servings">
                 <svg>
-                  <use href="${icons}#icon-minus-circle"></use>
+                  <use href="src/img/icons.svg#icon-minus-circle"></use>
                 </svg>
               </button>
               <button class="btn--tiny btn--increase-servings">
                 <svg>
-                  <use href="${icons}#icon-plus-circle"></use>
+                  <use href="src/img/icons.svg#icon-plus-circle"></use>
                 </svg>
               </button>
             </div>
@@ -75,7 +67,7 @@ const showRecipe = async function () {
 
           <div class="recipe__user-generated">
             <svg>
-              <use href="${icons}#icon-user"></use>
+              <use href="src/img/icons.svg#icon-user"></use>
             </svg>
           </div>
           <button class="btn--round">
@@ -86,14 +78,13 @@ const showRecipe = async function () {
         </div>
 
         <div class="recipe__ingredients">
-        <h2 class="heading--2">Recipe ingredients</h2>
+          <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
-           ${recipe.ingredients
-             .map((ing) => {
-               return `
+           ${recipe.ingredients.map((ing)=>{
+            return `
             <li class="recipe__ingredient">
               <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
+                <use href="src/img/icons.svg#icon-check"></use>
               </svg>
               <div class="recipe__quantity">${ing.quantity}</div>
               <div class="recipe__description">
@@ -102,8 +93,7 @@ const showRecipe = async function () {
               </div>
             </li>
             `;
-             })
-             .join("")}
+        }).join("")}
           </ul>
         </div>
 
@@ -121,17 +111,17 @@ const showRecipe = async function () {
           >
             <span>Directions</span>
             <svg class="search__icon">
-              <use href="${icons}#icon-arrow-right"></use>
+              <use href="src/img/icons.svg#icon-arrow-right"></use>
             </svg>
           </a>
         </div>
     `;
-    recipeContainer.innerHTML = "";
-    recipeContainer.insertAdjacentHTML("afterbegin", markup);
-  } catch (error) {
-    alert(error);
-  }
+        recipeContainer.innerHTML = "";
+        recipeContainer.insertAdjacentHTML("afterbegin", markup);
+    } catch (error) {
+        alert(error);
+    }
 };
-["hashchange", "load"].forEach((ev) => window.addEventListener(ev, showRecipe));
-// window.addEventListener("hashchange", showRecipe);
-// window.addEventListener("load", showRecipe);
+showRecipe();
+
+//# sourceMappingURL=starter.62406edb.js.map
